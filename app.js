013f9ae57940aeee816ee24dd26870889a747adf
robermau <robertom.perez@outlook.com>
2022-09-06 19:58:08 -0300

    secondt version

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer')
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 
 
 
 const main = async() => {
  
+
+    const busquedas = new Busquedas();
     let opt;
 
     do {
       opt = await inquirerMenu()
-      console.log({ opt })
+      
+      switch( opt ) {
+
+        case 1:
+            //Mostrar mensjae
+             const lugar = await leerInput('Ciudad: ');
+             console.log(lugar)
+            //Buscar los lugar
+
+            //Seleccionar el lugar
+
+            // Clima
+
+            // Mostrar resultados
+
+            console.log('\nInformacion de la ciudad\n'.green)
+            console.log('Ciudad:',)
+            console.log('Lat:',)
+            console.log('Lng:',)
+            console.log('Temperatura:',)
+            console.log('Minima:',)
+            console.log('Maxima:',)
+      }   break;
+
+
 
      if ( opt !== 0 ) await pausa()
 
---
+++ models/busquedas.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+class Busquedas {
+
+historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+
+constructor() {
+ // TODO : leer DB si existe
+
+
+}
+
+async ciudad ( lugar = '' ) {
+
+ // peticion http
+
+ console.log(lugar)
+
+ return []; // retornar los lugares que coincidan.
+
+}
+
+}
+
+
+
+module.exports = Busquedas;
